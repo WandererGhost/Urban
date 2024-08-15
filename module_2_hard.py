a5d3ea9c8ad7ee52cbcb14b12aae9c60893f97cf
WandererGhost <pv_tanuha@mail.ru>
2024-08-15 16:11:50 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,26 +1,37 @@
 import random
-
 the_trap = []
-key = []
 def i_am_ready():
     for i in range (3, 21):
         the_trap.append (i)
     n = random.choice(the_trap)
-    print ('Выпало ', n)
     return n
 n = i_am_ready()
-a = 1
-def checking_second_num():
-        for i in range (1, n):
-            check = a + i
-            result = n % check
-            if result == 0 and a != i:
-                key.append (a)
-                key.append (i)
 
-while a < n and a not in key:
+key = []
+a = 1
+
+def checking_second_num():
+    for i in range (1, n):
+        check = a + i
+        result = n % check
+        if result == 0 and a != i:
+            key.append (a)
+            key.append (i)
+
+def search_copy(key):
+    index = 1
+    index_ = 0
+    while index <= (len(key)-1):
+        if key[index_] != key[index]:
+            index = index + 1
+            index_ = index_ + 1
+        else:
+            break
+    return index
+
+while a < n:
     checking_second_num()
     a = a + 1
+index = search_copy (key)
 
-print (*key)
-print ('И да, пока я ломала голову над задачей меня уже мнооооооооооого раз раздавили шипы с потолка XD')
+print (n, '=', *key[0:index])
