c6e1b4e58b08adbd73be653af792bec465ccec1f
WandererGhost <pv_tanuha@mail.ru>
2024-08-18 12:46:18 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,37 +1,33 @@
 import random
+
 the_trap = []
+key = []
+
+
 def i_am_ready():
-    for i in range (3, 21):
-        the_trap.append (i)
+    for i in range(3, 21):
+        the_trap.append(i)
     n = random.choice(the_trap)
     return n
-n = i_am_ready()
 
-key = []
+
+n = i_am_ready()
 a = 1
 
+
 def checking_second_num():
-    for i in range (1, n):
+    for i in range(1, n):
         check = a + i
         result = n % check
-        if result == 0 and a != i:
-            key.append (a)
-            key.append (i)
+        if result == 0 and a != i and i not in key[0:n:1]:
+            key.append(a)
+            key.append(i)
 
-def search_copy(key):
-    index = 1
-    index_ = 0
-    while index <= (len(key)-1):
-        if key[index_] != key[index]:
-            index = index + 1
-            index_ = index_ + 1
-        else:
-            break
-    return index
 
 while a < n:
     checking_second_num()
     a = a + 1
-index = search_copy (key)
+#result_clean = len(key)//2       От этой схемы пришлось отказаться
+#result_clean = key[:result_clean]
 
-print (n, '=', *key[0:index])
+print(n, '=', *key)
