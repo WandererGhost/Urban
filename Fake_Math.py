3a5a6d096f25b5fe25f3468752707e6f3f8df9f1
WandererGhost <pv_tanuha@mail.ru>
2024-08-26 23:55:01 +0500

    Add files via upload

---
+++ Fake_Math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    div = None
+    if second == 0:
+        return 'Ошибка'
+    else:
+        div = first/second
+        return div
---
+++ True_Math.py
@@ -0,0 +1,8 @@
+def divide (first, second):
+    div = None
+    from math import inf
+    if second == 0:
+        return inf
+    else:
+        div = first / second
+        return div
