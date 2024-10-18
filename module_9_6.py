d47c3afd1c3ccf643667fb56edee884782bf47f1
WandererGhost <pv_tanuha@mail.ru>
2024-10-18 13:12:49 +0500

    Create module_9_6.py

---
+++ module_9_6.py
@@ -0,0 +1,13 @@
+def all_variants(text):
+    n = len(text)
+    
+    for start in range(n):
+        for end in range(start + 1, n + 1):
+            yield text[start:end]
+
+
+
+# Пример работы функции:
+a = all_variants("abc")
+for i in a:
+    print(i)
