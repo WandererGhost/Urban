422c8a4425e00e2779ff2542406f415f444680e3
WandererGhost <pv_tanuha@mail.ru>
2024-11-21 22:58:25 +0500

    Update suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,10 +1,10 @@
 import unittest
-import tests_12_2_CHANGED
+import tests_12_3
 
 tests_runner = unittest.TestSuite()
 
-tests_runner.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_2.RunnerTest))
-tests_runner.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_2.TournamentTest))
+tests_runner.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
+tests_runner.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
 
 
 tests_test_runner = unittest.TextTestRunner(verbosity=2)
